5e15b9bc43d0e8f20123756697732c67e6db2166
huynang23 <huynang09@gmail.com>
2022-08-03 22:39:12 +0700

    bai test cuoi khoa

---
+++ bai2/index.js
@@ -0,0 +1,12 @@
+const numberOneTriangle = (number) => {
+    Number.isInteger(number);
+    for (let i = 1; i <= number; i++) {
+      let string = '';
+      for (let j = 1; j <= i; j++) {
+        string += '*';
+      }
+      console.log(string);
+    }
+  };
+  
+  numberOneTriangle(3);
